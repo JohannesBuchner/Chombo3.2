fff304384a14c3218fcdceeaec0480b1899c84cf
ligocki <ligocki@c458f74e-99f2-40f8-9f0a-d519cfd9a9a7>
2014-01-23 03:30:55 +0000

    begin release 3.2...

---
+++ release/3.2/lib/src/BaseTools/CH_Thread.cpp
@@ -0,0 +1,27 @@
+#ifdef CH_LANG_CC
+/*
+ *      _______              __
+ *     / ___/ /  ___  __ _  / /  ___
+ *    / /__/ _ \/ _ \/  V \/ _ \/ _ \
+ *    \___/_//_/\___/_/_/_/_.__/\___/
+ *    Please refer to Copyright.txt, in Chombo's root directory.
+ */
+#endif
+
+#include <omp.h>
+#include "CH_Thread.H"
+#include "NamespaceHeader.H"
+
+bool onThread0()
+{
+  bool retval = true;
+#if CH_OPENMP==1
+  int thread_num = omp_get_thread_num();
+  retval = (thread_num== 0);
+#endif       
+  return retval;
+}
+
+
+
+#include "NamespaceFooter.H"
